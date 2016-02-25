96b79f1b817337b3b2c892b382c5f92a11742820
xiaoxue <lixue100000@sina.com>
2016-02-25 18:01:02 +0800

    Initial commit

---
+++ changeskinlib/src/main/java/com/lixue/aibei/changeskin/attr/SkinAttr.java
@@ -0,0 +1,10 @@
+package com.lixue.aibei.changeskin.attr;
+
+/**
+ * Created by Administrator on 2016/2/25.
+ */
+public class SkinAttr {
+    private String resName;
+//    private SkinAttrType attrType;
+
+}
